7684f283a7c3d5e087b84dd7126a59781de6b905
Home <yomi@xiaodingdangdebijibendiannao.local>
2026-07-21 21:27:28 +0800

    chore: update base path to /fund-ledger/v3/

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,3 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ base: '/fund-ledger/v2/', plugins: [react()] })
+export default defineConfig({ base: '/fund-ledger/v3/', plugins: [react()] })
